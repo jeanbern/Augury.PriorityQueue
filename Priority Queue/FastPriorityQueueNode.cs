325ae10b2a445090723660ee44f4a4cc57cec57f
JeanBernard P <jeanbernardp@pleasantsolutions.com>
2016-03-20 17:57:36 -0600

    Simplified for intended use.
    Removed the test and example projects.
    Changed classes to sealed.
    Removed methods that would go unused.
    Inlined a bit of code manually.
    Removed #if statements.
    Trivial resharper and bracketing updates.

--- Priority Queue/FastPriorityQueueNode.cs
+++ Priority Queue/FastPriorityQueueNode.cs
@@ -1,17 +1,13 @@
 ﻿namespace Priority_Queue
 {
-    public class FastPriorityQueueNode
+    internal sealed class FastPriorityQueueNode
     {
         /// <summary>
         /// The Priority to insert this node at.  Must be set BEFORE adding a node to the queue
         /// </summary>
         public double Priority { get; set; }
 
-        /// <summary>
-        /// <b>Used by the priority queue - do not edit this value.</b>
-        /// Represents the order the node was inserted in
-        /// </summary>
-        public long InsertionIndex { get; set; }
+        public string Word { get; set; }
 
         /// <summary>
         /// <b>Used by the priority queue - do not edit this value.</b>
