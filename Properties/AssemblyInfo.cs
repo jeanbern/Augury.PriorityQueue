e9547aa645cf95fefdeea92122243d354945ad46
Pellerin <JPelleri@epcor.ca>
2016-06-29 10:32:03 -0600

    Initial commit as Augury.PriorityQueue Contains a lot of changes to remove the generic capabilities. Adds a tie-breaker list of additional values beyond the original capacity.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Highly Optimized Priority Queue")]
-[assembly: AssemblyDescription("A highly optimized Priority Queue for path-finding and related applications")]
+[assembly: AssemblyTitle("PriorityQueue")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Highly Optimized Priority Queue")]
-[assembly: AssemblyCopyright("Copyright © BlueRaja 2013")]
+[assembly: AssemblyProduct("PriorityQueue")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3eee6b54-af8a-494b-9121-3d46ed09a58b")]
+[assembly: Guid("35d74c76-84fc-4a76-a1fc-4b279006e6d5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
